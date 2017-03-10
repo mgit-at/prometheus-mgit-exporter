d5e21bad227f4197d90b565a5f428ad52479478e
Christoph Hack <chack@mgit.at>
2017-03-10 11:26:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+prometheus-mgit-exporter
+========================
+
+A collection of useful monitoring for [Prometheus][1] by mgIT GmbH.
+
+[1]: https://prometheus.io/
+
+Available Checks
+----------------
+
+Each check must be enabled in the configuration file individually. The following
+checks are currently available:
+
+**certfile:** walks certain directories (like /etc/ssl, globbing configureable)
+and exports the "not after" field of all certificates found.
+
+Configuration
+-------------
+
+This is an example configuration file with all checks enabled:
+
+    {
+      "listen": ":9328",
+      "certfile": {
+        "enable": false,
+        "globs": [
+          "/etc/ssl/**/*.pem",
+          "/etc/ssl/**/*.crt"
+        ]
+      }
+    }
+
+License
+-------
+
+prometheus-mgit-exporter is distributed under the Apache License.
+See LICENSE for details.
+
+> Copyright 2017 mgIT GmbH.
+>
+> Licensed under the Apache License, Version 2.0 (the "License");
+> you may not use this file except in compliance with the License.
+> You may obtain a copy of the License at
+>
+>     http://www.apache.org/licenses/LICENSE-2.0
+>
+> Unless required by applicable law or agreed to in writing, > software
+> distributed under the License is distributed on an "AS IS" BASIS,
+> WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+> See the License for the specific language governing permissions and
+> limitations under the License.
---
+++ certfile.go
@@ -0,0 +1,96 @@
+// Copyright (c) 2017 mgIT GmbH. All rights reserved.
+// Distributed under the Apache License. See LICENSE for details.
+
+package main
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+
+	zglob "github.com/mattn/go-zglob"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type CertFileOptions struct {
+	Globs []string `json:"globs"`
+}
+
+type CertFileChecker struct {
+	opts CertFileOptions
+
+	promCertExpires *prometheus.Desc
+}
+
+func NewCertFileChecker(opts CertFileOptions) *CertFileChecker {
+	return &CertFileChecker{
+		opts: opts,
+		promCertExpires: prometheus.NewDesc(
+			"certfile_expires",
+			"Time when the certificate will expire",
+			[]string{"file", "cn"},
+			nil),
+	}
+}
+
+func (c *CertFileChecker) Describe(ch chan<- *prometheus.Desc) {
+	ch <- c.promCertExpires
+}
+
+func (c *CertFileChecker) Collect(ch chan<- prometheus.Metric) {
+
+	for _, glob := range c.opts.Globs {
+		matches, err := zglob.Glob(glob)
+		if err != nil {
+			log.Printf("failed to glob %q: %v", glob, err)
+			continue
+		}
+		for _, m := range matches {
+			if err := c.collectCert(m, ch); err != nil {
+				log.Printf("failed to check %q: %v", m, err)
+			}
+		}
+	}
+}
+
+func (c *CertFileChecker) collectCert(p string, ch chan<- prometheus.Metric) error {
+	if !filepath.IsAbs(p) {
+		absPath, err := filepath.Abs(p)
+		if err != nil {
+			return errors.Wrap(err, "failed to determine absolute path")
+		}
+		p = absPath
+	}
+	data, err := ioutil.ReadFile(p)
+	if err != nil {
+		return errors.Wrapf(err, "failed to read file")
+	}
+
+	block, _ := pem.Decode(data)
+
+	if block.Type != "CERTIFICATE" {
+		return nil
+	}
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return errors.Wrap(err, "failed to parse certificate")
+	}
+
+	if cert.NotAfter.IsZero() {
+		return nil
+	}
+
+	ch <- prometheus.MustNewConstMetric(
+		c.promCertExpires,
+		prometheus.GaugeValue,
+		float64(cert.NotAfter.Unix()),
+		p,
+		cert.Subject.CommonName,
+	)
+
+	return nil
+}
