b52d8b1fb27373299612b584d707345919b2a848
Maciej Krüger <mkrueger@mgit.at>
2024-03-22 22:57:07 +0100

    better method

--- module.nix
+++ module.nix
@@ -1,8 +1,8 @@
-{ config, pkgs, lib, options, utils, modulesPath, ... }@args:
-
-with (import "${modulesPath}/services/monitoring/prometheus/mk-exporter.nix" lib);
-
-{
-  options.services.prometheus.exporters = mkDownstreamOptions "mgit" (import ./mgit-exporter.nix args);
-  config = mkDownstreamConfig "mgit" (import ./mgit-exporter.nix args) config.services.prometheus.exporters.mgit;
+{ modulesPath, ... }: {
+  imports = [
+    (import "${modulesPath}/services/monitoring/prometheus/mk-downstream-exporter.nix" {
+      name = "mgit";
+      file = ./mgit-exporter.nix;
+    })
+  ];
 }
